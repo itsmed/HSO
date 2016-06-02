aca0f686bbb7407406e02a3a07a5fe7252d66385
DejonGill <dejonjgill@gmail.com>
2016-06-02 15:03:16 -0700

    Basic server and running and proxying. Webpack successful.

--- contributing.md
+++ contributing.md
@@ -19,6 +19,8 @@ Tickets and issues are in the waffle.io board. Please grab/submit tickets there,
 ####ES6
 Please use es6 syntax everywhere possible. ES6 features will be transpiled via Babel when webpack runs (npm start), and when es6 standards are implemented in browsers, refactoring code to use es6 syntax will be unnecessary. <br />
 As ES6 incorporates block scope amongst other things, variable naming should not be an issue. Still, please follow best practices and refrain from using global variables.
+####Back-end
+Please refrain from allowing back-end code from becoming full-on spaghetti. Adhere to best practices, and add functions for back-end to appropriately named files in server folder. In case es6 is not supported on server, please use module.exports/require rather than es6 import.
 ####Styling
 Styling will be done with Sass. <br />
 Please use .scss file extensions. Webpack will transpile .scss files into .css before applying the styles and bundling all js into one bundle.js <br />
@@ -29,5 +31,7 @@ Sass Mixins and functions should be in src/styles/sass-mix-func.scss This will a
 Please write tests using for all features created, ensuring they pass before submitting a pull request. <br />
 All features should be end-to-end tested. <br />
 Please keep all tests in test folder. The test folder should mimic the src folder exactly, with folders and file names of test files corresponding to what is being tested in src folder (ie: src/components/Store.js should be tested in test/components/Store.js).
-
+###Comments
+Please keep in mind that this project may be inherited by other devs, or that we ourselves on the current team may forget what aspects of code are supposed to do. <br />
+With that in mind, please comment code, following best practices.
 ####Thanks for reading, and happy hacking!
---
+++ server.js
@@ -0,0 +1,51 @@
+/*
+This is a node server using expressjs. If unfamailar with aspects of node servers, please checkout the docs at https://nodejs.org/en/docs/
+*/
+'use strict';
+const express = require('express');
+const path = require('path');
+// webpack will serve from the development server unless proxied, hence using http-proxy
+const httpProxy = require('http-proxy');
+// Webpack requires the path to our index.html file to be clearly given. index.html is located in the public folder of our root directory.
+const publicPath = path.resolve(__dirname, 'public');
+
+// If we are in production mode then webpack will serve from port 3500, otherwise it will find the port to serve from via the hosting environment 
+const isProduction = process.env.NODE_ENV === 'production';
+const port = isProduction ? process.env.PORT : 3500;
+// We need to add a configuration to our proxy server, as we are going to proxy outside of localhost
+const proxy = httpProxy.createProxyServer({
+  changeOrigin: true
+});
+
+// create our express application
+const app = express();
+
+// serving the index.html
+app.use(express.static(publicPath));
+
+// if not in production, this proxies requests to localhost:3000 and sendt them to our webpack server at localhost:8080
+if (!isProduction) {
+  const bundle = require('./server/compiler.js');
+  bundle();
+  // app.all is a special routing method used for loading middleware functions
+  app.all('/build/*', (req, res) => {
+    // check the request
+    console.log('request body', req.body);
+    proxy.web(req, res, {
+      target: 'http://localhost:8080'
+    });
+  });
+}
+proxy.on('error', (e) => {
+  console.warn('Could not connect to proxy, please try again. Error: ', e);
+});
+
+// spin up the server
+app.listen(port, () => {
+  console.log('Server running on port ' +port);
+});
+
+// catch all other GET requests, such as when refreshing
+app.get('*', (req, res) => {
+  res.sendFile(publicPath + '/index.html');
+});
